90eb817a9df2806f77610ca45312fcf3882d13e8
organicnz <tamerlanium@gmail.com>
2026-06-06 11:11:11 -0700

    Add ci_pre_xcodebuild.sh for plugin validation

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidations -bool YES
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
