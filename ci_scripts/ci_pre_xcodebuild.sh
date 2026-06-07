021e923943789a7ad54fab5ebbe26d12d8637613
organicnz <tamerlanium@gmail.com>
2026-06-07 00:06:19 -0700

    Fix AppSettings decoding, CI script typo, and Xcode Cloud code signing export

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidations -bool YES
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
-defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
